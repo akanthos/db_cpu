556980e020cf8552f4c7cae5da72ed065d1006f8
Athanasios Akanthos Chasapis <hasapisthanasis@gmail.com>
2015-04-21 04:10:02 +0200

    Implemented basic functionality and buffered output to final output file.

--- Exercise1/external_sort.cpp
+++ Exercise1/external_sort.cpp
@@ -1,77 +1,79 @@
 #include "external_sort.hpp"
 #include <algorithm>
+#include <queue> 
+#include <tuple> 
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <cstdint>
+#include <string>
+#include <sstream>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#define BUFFER_SIZE (65536) // 4MB in 64-bit numbers
 using namespace std;
 
 struct stat fileStat;
 uint64_t *buffer;
 uint64_t chunkSize;
+uint64_t *outputBuffer;
+uint64_t bfIndex = 0;
 
-// int write_to_file(int fd, char *buffer)
-// {
-// 	size_t len, idx;
-// 	ssize_t wcnt;
-// 	idx = 0;
-// 	len = strlen(buffer);
-// 	do {
-		
-// 		wcnt = write(fd,buffer + idx, len - idx);
-// 		if (wcnt == -1)
-// 		{ /* error */
-// 			perror("write");
-// 			return 1;
-// 		}
-// 		idx += wcnt;
-// 	} while (idx < len);
-// 	return 0;
-// }
-uint64_t getNextChunk(int fdInput) {
-	cout << "Reading chunk..." << endl;
-	//off_t position = lseek(fdInput, 0, SEEK_CUR);
-	cout << "Reading from " << fdInput << " at the buffer " << buffer << " a chunk " << chunkSize << endl;
-	uint64_t offset = read(fdInput, buffer, chunkSize);
-	cout << "Read " << offset << " bytes" << endl;
-	while (offset != chunkSize && offset != 0) {
-		offset += read(fdInput, buffer+offset, chunkSize);
+
+
+struct my_tuple {
+    uint64_t num;
+    uint64_t i;
+    bool operator<(const my_tuple& rhs) const
+    {
+        return num < rhs.num;
+    }
+};
+priority_queue<my_tuple> my_queue;
+
+void print_chunk(uint64_t elements) {
+	uint64_t *temp = buffer;
+	for (uint64_t i=0; i<elements; i++) {
+		cout << temp[i] << endl;
 	}
-	if (offset==0) {
-		// Reached end of file, return empty buffer
-		free(buffer);
-		buffer=NULL;
-		return 0;
-	}
-	//lseek(fdInput, position, SEEK_SET);
-	return offset;
 }
-void write_to_file(int fdOutput) {
-	cout << "Writing chunk..." << endl;
-	uint64_t data_written = write(fdOutput, buffer, chunkSize);
-	if (data_written == 0) {
-		perror("Error while writing the file");
-		exit(1);
+
+void flush(FILE *outFile) {
+	if (bfIndex > 0) {
+		cout << "FINALLY Writing to file" << endl;
+		unsigned elems = fwrite(outputBuffer, sizeof(uint64_t), bfIndex, outFile);
+		cout << "Wrote " << elems << " elements" << endl;
 	}
-	while (data_written != chunkSize) {
-		data_written += write(fdOutput, buffer, chunkSize);
+}
+void write_to_file(FILE * outFile, uint64_t n) {
+	cout << bfIndex << " <---> " << endl;
+	if (bfIndex < BUFFER_SIZE) {
+		outputBuffer[bfIndex++] = n;
 	}
-	
+	else {
+		cout << "Writing to file" << endl;
+		fwrite(outputBuffer, sizeof(uint64_t), BUFFER_SIZE, outFile);
+		memset(outputBuffer, 0, BUFFER_SIZE*sizeof(uint64_t));
+		bfIndex = 0;
+		outputBuffer[bfIndex] = n;
+	}
+}
+
+uint64_t* getNextElement(FILE *f) {
+	uint64_t *temp = (uint64_t *) malloc(sizeof(uint64_t));
+	uint64_t e = fread(temp, sizeof(uint64_t), 1, f);
+	if (e==0) return NULL;
+	else return temp;
 }
 
 void external_sort(int fdInput, uint64_t size, int fdOutput, uint64_t memSize) {
-
-
-	FILE *inFile = fdopen(fdInput, "r");
-	FILE *outFile = fdopen(fdOutput, "w");
 
 	cout << "Size of buffer: " << memSize << endl;
 	// Begin sorting
 	chunkSize = (memSize / sizeof(uint64_t));
 	cout << "Size of actual buffer: " << chunkSize << " elements" << endl;
-    unsigned chunk_number;
+    uint64_t chunk_number;
     if (size < chunkSize)
     	chunk_number = 1;
     else {
@@ -81,16 +83,94 @@ void external_sort(int fdInput, uint64_t size, int fdOutput, uint64_t memSize) {
     cout << "Filesize in elements: " << size << endl;
 	cout << "Numbers of chunks: " << chunk_number << endl;
 
+	// Opening necessary files
+	FILE *inFile = fdopen(fdInput, "r");
+	// FILE *temp_file = fopen("temp.txt", "w");
+	FILE *outFile = fdopen(fdOutput, "w");
+	FILE *files[chunk_number];
+
+	
+
 	// Allocate memory buffer
-
 	uint64_t* buffer = (uint64_t *) malloc(chunkSize * sizeof(uint64_t));
-	uint64_t elements_read;
-	while ( elements_read = fread(buffer, sizeof(uint64_t), chunkSize, inFile)) {
-		cout << "Read " << elements_read << " elements" << endl;
-		fwrite(buffer, sizeof(uint64_t), elements_read, outFile);
-	}
 
+	// Read chunks and short them separately
+	uint64_t i = 0;
+	uint64_t elements_read;
+	while ( ( elements_read = fread(buffer, sizeof(uint64_t), chunkSize, inFile) ) ) {
+		std::stringstream s;
+		s << "tmp" << i;
+		string name = s.str();
+		files[i] = fopen(name.c_str(), "w");
+		cout << "Read " << elements_read << " elements" << endl;
+		// print_chunk(elements_read);
+		//std::sort(buffer, buffer+chunkSize, [](uint64_t a, uint64_t b) {  return a < b; } );
+		fwrite(buffer, sizeof(uint64_t), elements_read, files[i]);
+		fclose(files[i]);
+		++i;
+	}
 	free(buffer);
 	fclose(inFile);
+	// fclose(temp_file);
+
+	// Merging chunks
+
+	for (uint64_t i=0; i<chunk_number; i++) {
+		std::stringstream s;
+		s << "tmp" << i;
+		string name = s.str();
+		files[i] = fopen(name.c_str(), "r");
+	}
+
+
+	outputBuffer = (uint64_t *) malloc(BUFFER_SIZE*sizeof(uint64_t));
+	memset(outputBuffer, 0, BUFFER_SIZE*sizeof(uint64_t));
+	for (uint64_t i=0; i<chunk_number; i++) {
+		uint64_t * temp;
+		while ( (temp = getNextElement(files[i]) ) ) {
+			write_to_file(outFile, *temp);
+		}
+
+	}
+	
+	// for (uint64_t i=0; i<chunk_number; i++) {
+	// 	uint64_t *temp = getNextElement(files[i]);
+	// 	if (temp) {
+	// 		my_tuple elem = {*temp, i};
+	// 		my_queue.push(elem);
+	// 		free(temp);
+	// 	}
+
+	// }	
+
+	// outputBuffer = (uint64_t *) malloc(BUFFER_SIZE*sizeof(uint64_t));
+	// memset(outputBuffer, 0, BUFFER_SIZE*sizeof(uint64_t));
+	// my_tuple temp;
+	// while (!my_queue.empty()) {
+	// 	// Remove min element
+	// 	temp = my_queue.top();
+	// 	my_queue.pop();
+		
+	// 	// Write it to output file
+	// 	write_to_file(outFile, temp.num);
+		
+	// 	// Bring next element from input file piece 
+	// 	uint64_t *t = getNextElement(files[temp.i]);
+	// 	if (t) {
+	// 		my_tuple elem = {*t, temp.i};
+	// 		my_queue.push(elem);
+	// 		free(t);
+	// 	}
+	// }
+	
+	flush(outFile);
+	free(outputBuffer);
+	for (uint64_t i=0; i<chunk_number; i++) {
+		fclose(files[i]);
+		std::stringstream s;
+		s << "tmp" << i;
+		string name = s.str();
+		remove(name.c_str());
+	}
 	fclose(outFile);
 }
--- Exercise1/test.cpp
+++ Exercise1/test.cpp
@@ -3,24 +3,30 @@
 #include <iostream>
 #include "external_sort.hpp"
 
+#define G4 (4294967296) // 4GB in bytes
+#define G25 (41943040) // 2,5 GB in 64-bit numbers
+#define G5 (167772160) // 5 GB in 64-bit numbers
 using namespace std;
 
-uint64_t determine_system_memory() {
-	return 4*1024*1024;
-}
-
-int main() {
-	cout << "Test program!" << endl;
-	int inFile = open("in.txt", O_RDONLY);
-	int outFile = open("out.txt", O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+int main(int argc, char* argv[]) {
+	if (argc < 4) {
+		std::cerr << "usage: " << argv[0] << " <inputFile> <outputFile> <memoryBufferInMB>" << std::endl;
+		return -1;
+	}
+	uint64_t bytes = strtol(argv[3], NULL, 10);
+	cout << "user gave " << bytes << " bytes" << endl;
+	
+	int inFile = open(argv[1], O_RDONLY);
+	int outFile = open(argv[2], O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
 	if (inFile < 0 || outFile < 0) {
         perror("File does not exist");
 		exit (1);
 	}
 
-	// fseek(in, 0, SEEK_END);
-	// uint64_t inFileSize = ftell(in) / sizeof(uint64_t);
-	// fseek(in, 0, SEEK_SET);
-	uint64_t inFileSize = 10;
-	external_sort(inFile, inFileSize, outFile, determine_system_memory());
+	uint64_t inFileSize;
+	inFileSize = lseek(inFile, 0, SEEK_END) / sizeof(uint64_t);
+	lseek(inFile, 0, 0);
+
+	
+	external_sort(inFile, inFileSize, outFile, bytes);
 }
